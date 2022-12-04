015f9685e280cde38d496880a98461f1855d15f1
Pratham Gupta <87975651+PrathamGupta06@users.noreply.github.com>
2022-12-04 13:37:30 +0530

    Modify it to read ids to skip from a file, show it more beautifully

--- winget-skip-upgrades.ps1
+++ winget-skip-upgrades.ps1
@@ -1,3 +1,4 @@
+# Write the software ids to skip in winget-skip-upgrades.txt file
 class Software {
     [string]$Name
     [string]$Id
@@ -5,7 +6,7 @@ class Software {
     [string]$AvailableVersion
 }
 
-$upgradeResult = winget upgrade | Out-String
+$upgradeResult = winget upgrade | Out-String 
 
 $lines = $upgradeResult.Split([Environment]::NewLine)
 
@@ -39,27 +40,32 @@ For ($i = $fl + 1; $i -le $lines.Length; $i++)
         $software.Id = $id;
         $software.Version = $version
         $software.AvailableVersion = $available;
-
+        $software.Id = $Software.Id -replace '[^a-zA-Z0-9.]', '' # Remove special chars
         $upgradeList += $software
     }
 }
 
+# Show all the software that can be upgraded
+# $upgradeList | Format-Table
 
-$upgradeList | Format-Table
+$toSkip = Get-Content -Path "winget-skip-upgrades.txt" -Encoding UTF8
 
-$toSkip = @(
-'Adobe.Acrobat.Reader.64-bit'
-)
+# Print the list of packages present in the upgradelist and toskip
+Write-Host "These Packages can be upgraded but will be skipped"
+$upgradeList | Where-Object { $toSkip.Contains($_.Id) } | Format-Table
 
-foreach ($package in $upgradeList) 
+# Print the list of packages present in the upgradelist and not toskip
+$toUpgrade = $upgradeList | Where-Object { -not $toSkip.Contains($_.Id) }
+Write-Host "These Packages will be upgraded"
+$toUpgrade | Format-Table
+
+foreach ($package in $toUpgrade) 
 {
-    if (-not ($toSkip -contains $package.Id)) 
-    {
-        Write-Host "Going to upgrade package $($package.id)"
-        & winget upgrade $package.id
-    }
-    else 
-    {    
-        Write-Host "Skipped upgrade to package $($package.id)"
-    }
+    Write-Host "====== Upgrading $($package.id) ======"
+    Write-Host "Current version: $($package.Version)"
+    Write-Host "Available version: $($package.AvailableVersion)"
+    & winget upgrade $package.id
+    $endline = "-" * 40
+    Write-Host $endline
+    Write-Host ""
 }
